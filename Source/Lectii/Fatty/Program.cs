564a95f4d862ccbade424099a8bf5318bdbdc0f5
Hexodust <PGTanase@gmail.com>
2023-11-10 17:11:28 +0200

    Adaugarea proiectului Fatty in structura de git locala

---
+++ Source/Lectii/Fatty/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Person
+{
+    public string Name;
+    public string Address;
+    public int Age;
+    public char Sex;
+    public string Eye_color;
+
+    public Person (string Name, string Address, int Age, char Sex, string Eye_color )
+    {
+        this.Name = Name;
+        this.Address = Address;
+        this.Age = Age;
+        this.Sex = Sex;
+        this.Eye_color = Eye_color;
+    }
+}
